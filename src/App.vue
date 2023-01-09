cd3959ca9b7c4be6a41266c5f37092390011cd03
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2023-01-08 20:34:39 -0500

    add puntos in lenguajes and events computed

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,16 @@
       <input type="text" v-model="nuevoLenguaje" @keyup.enter="agregarLenguaje" />
       <input type="text" v-model="nuevoLink" @keyup.enter="agregarLenguaje" />
       <button @click="agregarLenguaje">Agregar lenguaje</button>
+
     </div>
     <ul>
       <li v-for="lenguaje of lenguajes" :key="lenguaje.nombre">
-        <a :href="lenguaje.link" target="_blank">{{ lenguaje.nombre }}</a>
+        <a :href="lenguaje.link" target="_blank">{{ lenguaje.nombre }}</a> {{ lenguaje.puntos }}
+        <button @click="lenguaje.puntos++">+</button>
+        <button @click="lenguaje.puntos--">-</button>
       </li>
     </ul>
+    Suma de todos los votos {{ sumarTodo }}
   </div>
 </template>
 
@@ -18,25 +22,33 @@
 
 export default {
   name: 'App',
-  data: function () {
-    return {
-      contenido: "Hola mundo desde programador novato",
-      lenguajes: [
-        { nombre: "JavaScript", link: 'https://www.javascript.com', puntos: 0 },
-        { nombre: "PHP", link: 'https://php.net', puntos: 0 },
-        { nombre: "Python", link: 'https://www.python.org', puntos: 10 }
-      ],
-      nuevoLenguaje: '',
-      nuevoLink: '',
-    };
-  },
+  data: () => ({
+    contenido: "Hola mundo desde programador novato",
+    lenguajes: [
+      { nombre: "JavaScript", link: 'https://www.javascript.com', puntos: 0 },
+      { nombre: "PHP", link: 'https://php.net', puntos: 0 },
+      { nombre: "Python", link: 'https://www.python.org', puntos: 0 }
+    ],
+    nuevoLenguaje: '',
+    nuevoLink: ''
+  }),
   methods: {
     agregarLenguaje() {
-      this.lenguajes.push({ nombre: this.nuevoLenguaje, link: this.nuevoLink })
+      this.lenguajes.push({ nombre: this.nuevoLenguaje, link: this.nuevoLink, puntos: 0 })
       this.nuevoLenguaje = '';
       this.nuevoLink = '';
-    }
+    },
   },
+  computed: {
+    sumarTodo() {
+      let total = 0;
+      this.lenguajes.forEach( lenguaje => {
+        total += lenguaje.puntos;
+      })
+      
+      return total;
+    }
+  }
 }
 </script>
 
